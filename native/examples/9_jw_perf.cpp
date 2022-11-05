70e3104c2acd66c24c9f37750d5c63ab005d7966
jw-lenovo <jjwhitham@gmail.com>
2022-11-05 15:27:09 +1100

    Add timer

--- native/examples/9_jw_perf.cpp
+++ native/examples/9_jw_perf.cpp
@@ -2,9 +2,17 @@
 // Licensed under the MIT license.
 
 #include "examples.h"
+#include <sys/time.h>
 
 using namespace std;
 using namespace seal;
+
+// Return current wallclock time, for performance measurement
+inline uint64_t GetTimeStamp() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec * (uint64_t)1000000 + tv.tv_usec;
+}
 
 void jw_perf()
 {
